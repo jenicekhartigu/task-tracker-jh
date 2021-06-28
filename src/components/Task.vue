2caab3246c8857af825d4ee95cdd1225bd8ef8f6
jenicekhartigu <honza.hartig@seznam.cz>
2021-06-28 18:31:39 +0200

    Task Tracker

---
+++ src/components/Task.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
+    <h3>
+      {{ task.text }}
+      <i
+        @click="$emit('delete-task', task.id)"
+        class="fas fa-skull-crossbones"
+      ></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scope>
+.fas {
+  color: red;
+}
+
+.fas:hover {
+  color: #940000;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task:hover {
+  background: #e6e6e6;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
